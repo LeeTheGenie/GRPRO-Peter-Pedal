812551a497035f4600a26666130a5c9babc95aaf
Markus Sværke Staael <markus@staael.com>
2023-11-21 12:47:43 +0100

    inital
    .

---
+++ BlueJ/testPerson.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+public class testPerson
+{
+    @BeforeEach public void setUp() {
+        
+    }
+    @Test public void testPerson() {
+        
+        //assertEquals(0, car.getSpeed());
+    }
+    @AfterEach public void tearDown() {
+        
+    }
+}
