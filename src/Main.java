2b9abd43da5f162adf430bfe1ea42312494b86a3
YeChengde <nickyye@hotmail.dk>
2023-11-21 16:29:06 +0100

    qwe

--- src/Main.java
+++ src/Main.java
@@ -22,13 +22,13 @@ public class Main {
         World world = p.getWorld();
 
         // Display information
-            // Grass
-        p.setDisplayInformation(Grass.class, new DisplayInformation(Color.green,"grass",true));
-            // Flower
-        p.setDisplayInformation(Flower.class, new DisplayInformation(Color.yellow,"flower",false));
+        // Grass
+        p.setDisplayInformation(Grass.class, new DisplayInformation(Color.green, "grass", true));
+        // Flower
+        p.setDisplayInformation(Flower.class, new DisplayInformation(Color.yellow, "flower", false));
 
-            // Rabbit
-        DisplayInformation di = new DisplayInformation(Color.red);
+        // Rabbit
+        DisplayInformation di = new DisplayInformation(Color.black, "rabbit-small");
         p.setDisplayInformation(Rabbit.class, di);
 
         // Set grass
@@ -37,34 +37,6 @@ public class Main {
         p.show();
 
         for (int i = 0; i < 3000; i++) {
-            p.simulate();
-        }
-    }
-
-    public static void setupAndRunSimulation2() {
-        int size = 15;
-        int delay = 1000;
-        int display_size = 800;
-        int amount = 10;
-        Random r = new Random();
-        for (int i = 0; i < amount; i++) {
-            int x = r.nextInt(size);
-            int y = r.nextInt(size);
-            Location l = new Location(x, y);
-            // Så længe pladsen ikke er tom, forsøger vi med en ny tilfældig plads:
-            while (!world.isTileEmpty(l)) {
-                x = r.nextInt(size);
-                y = r.nextInt(size);
-                l = new Location(x, y);
-            }
-            // og herefter kan vi så anvende den:
-            world.setTile(l, new Rabbit());
-        }
-
-        // Show
-        p.show();
-
-        for (int i = 0; i < 200; i++) {
             p.simulate();
         }
     }
