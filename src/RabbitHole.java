f6384684d3988de1f2a8bc04cd124e2eb39281b7
YeChengde <nickyye@hotmail.dk>
2023-11-25 12:56:44 +0100

    ik noget

--- src/RabbitHole.java
+++ src/RabbitHole.java
@@ -4,51 +4,50 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class RabbitHole extends LivingBeing implements NonBlocking {
-    
+
     ArrayList<Object> rabbits;
 
-    @Override public void act(World world) {
+    @Override
+    public void act(World world) {
         keepRabbit(world);
         exitRabbit(world);
     }
 
-    @Override public RabbitHole newInstance() {
+    @Override
+    public RabbitHole newInstance() {
         return new RabbitHole();
     }
 
-    RabbitHole(){
-        super(0,0);
+    RabbitHole() {
+        super(0, 0);
         this.rabbits = new ArrayList<>();
     }
 
-    public void keepRabbit(World world){
-        if(world.isNight()==true){
-            if(world.getTile(world.getLocation(this)) instanceof Rabbit){
+    public void keepRabbit(World world) {
+        if (world.isNight() == true) {
+            if (world.getTile(world.getLocation(this)) instanceof Rabbit) {
                 Object rabbit = world.getTile(world.getLocation(this));
                 this.rabbits.add(rabbit);
                 world.remove(rabbit);
 
-                System.out.println("Rabbits resting: "+this.rabbits);
-                System.out.println("In this rabbithole: "+world.getTile(world.getLocation(this)));
+                System.out.println("Rabbits resting: " + this.rabbits);
+                System.out.println("In this rabbithole: " + world.getTile(world.getLocation(this)));
             }
         }
     }
 
-    public void exitRabbit(World world){
-        try{
-        if(world.isDay()==true){
-            for(Object rabbit:rabbits){
-                
-                
-                
-                System.out.println(rabbit+" left "+this);
-                this.rabbits.remove(rabbit);
+    public void exitRabbit(World world) {
+        try {
+            if (world.isDay() == true) {
+                for (Object rabbit : rabbits) {
+
+                    System.out.println(rabbit + " left " + this);
+                    this.rabbits.remove(rabbit);
+                }
             }
-        }
-        }catch(Exception e){
+        } catch (Exception e) {
 
         }
     }
-    
 
 }
