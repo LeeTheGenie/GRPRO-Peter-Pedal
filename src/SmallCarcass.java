224a90146b70bd2f3e5c409715e5fdbf537e9267
YeChengde <nickyye@hotmail.dk>
2023-12-04 14:18:31 +0100

    smallcarcass

---
+++ src/SmallCarcass.java
@@ -0,0 +1,20 @@
+
+import abstracts.LivingBeing;
+import itumulator.world.NonBlocking;
+import itumulator.world.World;
+
+public class SmallCarcass extends LivingBeing implements NonBlocking {
+
+    @Override
+    public void act(World world) {
+    }
+
+    @Override
+    public SmallCarcass newInstance() {
+        return new SmallCarcass();
+    }
+
+    public SmallCarcass() {
+        super(0, 0);
+    }
+}
